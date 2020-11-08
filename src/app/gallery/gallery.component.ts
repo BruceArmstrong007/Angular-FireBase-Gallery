261ee7349769dea9c452b7cf215f652917cd59e2
Bruce <bruce.armstrong09@gmail.com>
2020-11-08 18:38:03 +0530

    Delete Functionally Updated

--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { AngularFirestore } from "@angular/fire/firestore";
 import { Router,ActivatedRoute } from '@angular/router';
 import { GalleryService } from './gallery.service';
+import { AngularFireStorage } from '@angular/fire/storage';
 @Component({
   selector: 'app-gallery',
   templateUrl: './gallery.component.html',
@@ -13,7 +14,9 @@ export class GalleryComponent implements OnInit {
   Name : string;
   downloadURL: string;
   documentPhotos;
-  constructor(private firebase : AngularFirestore,private router : Router,private route : ActivatedRoute,private gallery : GalleryService) { }
+  basepath : string;
+  ref;
+  constructor(private firebase : AngularFirestore,private router : Router,private firestorage : AngularFireStorage,private route : ActivatedRoute,private gallery : GalleryService) { }
 
   ngOnInit(): void {
 
@@ -21,12 +24,21 @@ export class GalleryComponent implements OnInit {
 
     this.firebase.collection('users').doc(this.id).collection('photos').get().subscribe(res =>{
       this.documentPhotos = res.docs;
+      console.log(res.docs);
       });
+
+
   }
 
-  deleteData(imgId:string){
+  deleteData(imgId:string,imgName:string){
+    this.basepath = 'images/'+this.id+'/';
+    this.ref = this.firestorage.ref(this.basepath);
     this.firebase.collection('users').doc(this.id).collection('photos').doc(imgId).delete().then(res => {
-      window.location.reload();
+      console.log(res);
+      this.ref.child(imgName).delete().subscribe(res => {
+         console.log(res);
+         window.location.reload();
+       });
     });
   }
 
